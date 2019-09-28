6ee12c566bd770a3a75c41ccbf2f41d22d714d41
Mikhailosev <mikhail.losev2@gmail.com>
2019-09-29 01:20:48 +0300

    fullpost

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import connect from "@vkontakte/vk-connect";
 import {
-    View,
-    Epic,
-    Tabbar,
-    TabbarItem,
-    Group,
-    ModalRoot,
-    ModalPage,
-    ModalPageHeader,
-    IS_PLATFORM_ANDROID, HeaderButton, IS_PLATFORM_IOS, FormLayoutGroup, Button
+  View,
+  Epic,
+  Tabbar,
+  TabbarItem,
+  Group,
+  ModalRoot,
+  ModalPage,
+  ModalPageHeader,
+  IS_PLATFORM_ANDROID,
+  HeaderButton,
+  IS_PLATFORM_IOS,
+  FormLayoutGroup,
+  Button
 } from "@vkontakte/vkui";
 import "@vkontakte/vkui/dist/vkui.css";
 import Icon28FavoriteOutline from "@vkontakte/icons/dist/28/favorite_outline";
@@ -30,9 +34,8 @@ import Navigation from "./panels/Navigation.js";
 import VKStories from "vk-stories";
 import VKConnect from "@vkontakte/vkui-connect-promise";
 import FormLayout from "@vkontakte/vkui/dist/components/FormLayout/FormLayout";
-import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
-import Icon24Dismiss from '@vkontakte/icons/dist/24/dismiss';
-
+import Icon24Cancel from "@vkontakte/icons/dist/24/cancel";
+import Icon24Dismiss from "@vkontakte/icons/dist/24/dismiss";
 
 class App extends React.Component {
   constructor(props) {
@@ -50,9 +53,9 @@ class App extends React.Component {
       activeModal: null
     };
     this.onStoryChange = this.onStoryChange.bind(this);
-      this.modalBack = () => {
-          this.toggleModal();
-      };
+    this.modalBack = () => {
+      this.toggleModal();
+    };
   }
 
   componentDidMount() {
@@ -61,36 +64,36 @@ class App extends React.Component {
     console.log(this.state);
   }
 
-    async apiRequests() {
-        const user = await VKConnect.send("VKWebAppGetUserInfo", {}).catch(
-            () => console.log("Cannot get User information")
-        );
-        this.setState({fetchedUser: user.data});
+  async apiRequests() {
+    const user = await VKConnect.send("VKWebAppGetUserInfo", {}).catch(() =>
+      console.log("Cannot get User information")
+    );
+    this.setState({ fetchedUser: user.data });
 
-        const token = await VKConnect.send("VKWebAppGetAuthToken", {
-            app_id: this.state.appId,
-            scope: "groups,stories"
-        }).catch(
-            () => console.log("Cannot get Authorization Token")
-        );
-        this.setState({token: token.data.access_token, tokenScope: token.data.scope});
+    const token = await VKConnect.send("VKWebAppGetAuthToken", {
+      app_id: this.state.appId,
+      scope: "groups,stories"
+    }).catch(() => console.log("Cannot get Authorization Token"));
+    this.setState({
+      token: token.data.access_token,
+      tokenScope: token.data.scope
+    });
 
-        const groups = await VKConnect.send("VKWebAppCallAPIMethod", {
-            method: "groups.get",
-            request_id: "getGroups",
-            params: {
-                filter: "admin,editor,moder",
-                user_id: this.state.fetchedUser.id,
-                extended: "1",
-                fields: "description",
-                count: "999",
-                v: "5.101",
-                access_token: this.state.token}
-        }).catch(
-            () => console.log("Cannot get groups")
-        );
-        this.setState({groups: groups.data});
-    }
+    const groups = await VKConnect.send("VKWebAppCallAPIMethod", {
+      method: "groups.get",
+      request_id: "getGroups",
+      params: {
+        filter: "admin,editor,moder",
+        user_id: this.state.fetchedUser.id,
+        extended: "1",
+        fields: "description",
+        count: "999",
+        v: "5.101",
+        access_token: this.state.token
+      }
+    }).catch(() => console.log("Cannot get groups"));
+    this.setState({ groups: groups.data });
+  }
 
   // async apiRequests() {
   //   this.state.fetchedUser = await connect.sendPromise(
@@ -125,119 +128,149 @@ class App extends React.Component {
     this.setState({ activeStory: e.currentTarget.dataset.story });
   }
 
-    toggleModal() {
-        this.state.activeModal ? this.setState({activeModal:null}) : this.setState({activeModal:'share'})
-    }
+  toggleModal() {
+    this.state.activeModal
+      ? this.setState({ activeModal: null })
+      : this.setState({ activeModal: "share" });
+  }
 
-    getModalContent() {
-        return (
-            <ModalRoot activeModal={this.state.activeModal}>
-                <ModalPage
-                    id='share'
-                    header={
-                        <ModalPageHeader
-                            left={IS_PLATFORM_ANDROID && <HeaderButton onClick={this.modalBack}><Icon24Cancel /></HeaderButton>}
-                            right={IS_PLATFORM_IOS && <HeaderButton onClick={this.modalBack}><Icon24Dismiss /></HeaderButton>}
-                        >
-                            Поделиться
-                        </ModalPageHeader>
-                    }
-                    onClose={this.modalBack}
-                    settlingHeight={80}
-                >
-                    <FormLayout>
-                        <FormLayoutGroup>
-                            <Button level="secondary" onClick={() => this.postToWall()} size="xl">Опубликовать на стене</Button>
-                            <Button level="secondary" onClick={() => this.addToStory()} size="xl">Добавить в историю</Button>
-                        </FormLayoutGroup>
-                    </FormLayout>
-                </ModalPage>
-            </ModalRoot>
-        )
-    }
+  getModalContent() {
+    return (
+      <ModalRoot activeModal={this.state.activeModal}>
+        <ModalPage
+          id="share"
+          header={
+            <ModalPageHeader
+              left={
+                IS_PLATFORM_ANDROID && (
+                  <HeaderButton onClick={this.modalBack}>
+                    <Icon24Cancel />
+                  </HeaderButton>
+                )
+              }
+              right={
+                IS_PLATFORM_IOS && (
+                  <HeaderButton onClick={this.modalBack}>
+                    <Icon24Dismiss />
+                  </HeaderButton>
+                )
+              }
+            >
+              Поделиться
+            </ModalPageHeader>
+          }
+          onClose={this.modalBack}
+          settlingHeight={80}
+        >
+          <FormLayout>
+            <FormLayoutGroup>
+              <Button
+                level="secondary"
+                onClick={() => this.postToWall()}
+                size="xl"
+              >
+                Опубликовать на стене
+              </Button>
+              <Button
+                level="secondary"
+                onClick={() => this.addToStory()}
+                size="xl"
+              >
+                Добавить в историю
+              </Button>
+            </FormLayoutGroup>
+          </FormLayout>
+        </ModalPage>
+      </ModalRoot>
+    );
+  }
 
-    postToWall() {
-        VKConnect.send("VKWebAppShowWallPostBox", {
-            "message": "Hello!",
-            "v": "5.101"})
-            .then(response => console.log(response))
-            .catch(() => console.log("Can't post"))
-    }
+  postToWall() {
+    VKConnect.send("VKWebAppShowWallPostBox", {
+      message: "Hello!",
+      v: "5.101"
+    })
+      .then(response => console.log(response))
+      .catch(() => console.log("Can't post"));
+  }
 
-    addToStory() {
-        const VK_API_VERSION = "5.95";
-        const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-        const fields = [
-            {
-                x: 540,
-                y: 1133,
-                value: "Hello World",
-                font: "96px Arial",
-                align: "center",
-                color: "#FFFFFF"
-            }
-        ];
+  addToStory() {
+    const VK_API_VERSION = "5.95";
+    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+    const fields = [
+      {
+        x: 540,
+        y: 1133,
+        value: "Hello World",
+        font: "96px Arial",
+        align: "center",
+        color: "#FFFFFF"
+      }
+    ];
 
-        const __makeFakeFile = (dataUrl, filename) => {
-            let arr = dataUrl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-                bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-            while(n--) u8arr[n] = bstr.charCodeAt(n);
-            return new File([u8arr], filename, { type: mime });
-        };
+    const __makeFakeFile = (dataUrl, filename) => {
+      let arr = dataUrl.split(","),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+      while (n--) u8arr[n] = bstr.charCodeAt(n);
+      return new File([u8arr], filename, { type: mime });
+    };
 
-        let story;
+    let story;
 
-        VKStories.generateStoryFromTemplate(require("./img/persik.png"), fields)
-            .then(generatedStory => {
-                story = generatedStory;
-            })
-            .catch(() => console.log("Failed to generate story"));
+    VKStories.generateStoryFromTemplate(require("./img/persik.png"), fields)
+      .then(generatedStory => {
+        story = generatedStory;
+      })
+      .catch(() => console.log("Failed to generate story"));
 
-        VKConnect.send("VKWebAppCallAPIMethod", {
-            method: "stories.getPhotoUploadServer",
-            params: {
-                access_token: this.state.token,
-                add_to_news: "1",
-                link_text: "open",
-                link_url: "https://vk.com/app" + this.state.appId,
-                v: VK_API_VERSION
-            }
-        }).then((response) => {
-            const uploadUrl = response.data.response.upload_url;
+    VKConnect.send("VKWebAppCallAPIMethod", {
+      method: "stories.getPhotoUploadServer",
+      params: {
+        access_token: this.state.token,
+        add_to_news: "1",
+        link_text: "open",
+        link_url: "https://vk.com/app" + this.state.appId,
+        v: VK_API_VERSION
+      }
+    })
+      .then(response => {
+        const uploadUrl = response.data.response.upload_url;
 
-            const request = new FormData();
-            request.append("file", __makeFakeFile(story, "story.png"));
+        const request = new FormData();
+        request.append("file", __makeFakeFile(story, "story.png"));
 
-            console.log(uploadUrl, request);
+        console.log(uploadUrl, request);
 
-            fetch(CORS_PROXY + uploadUrl, {
-                method: "POST",
-                body: request
-            })
-                .then((response) => response.json())
-                .then((response) => console.log(response.response.story))
-                .catch(() => console.log("Can't upload story"))
+        fetch(CORS_PROXY + uploadUrl, {
+          method: "POST",
+          body: request
         })
-            .catch(() => console.log("Can't get upload url"));
-        this.toggleModal();
-    }
+          .then(response => response.json())
+          .then(response => console.log(response.response.story))
+          .catch(() => console.log("Can't upload story"));
+      })
+      .catch(() => console.log("Can't get upload url"));
+    this.toggleModal();
+  }
 
-    handlePayButtonClick() {
-        const receivingUserId = 7150594;
-        console.log("i am here in handler");
-        VKConnect.send("VKWebAppOpenPayForm", {
-            "app_id": this.state.appId,
-            "action": "pay-to-user",
-            "params": {
-                "amount": 100.0,
-                "description": "Payment for post",
-                "user_id": receivingUserId
-            }
-        }).then(response => console.log(response.data))
-            .catch(err => console.log(err));
-    }
-
-
+  handlePayButtonClick() {
+    const receivingUserId = 7150594;
+    console.log("i am here in handler");
+    VKConnect.send("VKWebAppOpenPayForm", {
+      app_id: this.state.appId,
+      action: "pay-to-user",
+      params: {
+        amount: 100.0,
+        description: "Payment for post",
+        user_id: receivingUserId
+      }
+    })
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
