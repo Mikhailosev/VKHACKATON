3335e3fd589ed70a07eb8817945c0c810c7bb93c
Seva <sevastyan@rabdano.ru>
2019-09-29 03:54:01 +0300

    Update some styles

--- client/src/panels/FullPost.js
+++ client/src/panels/FullPost.js
@@ -50,7 +50,10 @@ class FullPost extends React.Component {
             opacity: "1"
           }}
         >
-            <PanelHeaderBack onClick={() => this.props.history.goBack()}/>
+            <PanelHeaderBack
+                onClick={() => this.props.history.goBack()}
+                style={{color: "white", marginLeft: "3px"}}
+            />
         </div>
 
           <h1 style={{
